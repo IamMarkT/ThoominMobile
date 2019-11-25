15edb48495ec8914420185fcd096c459a2dcd536
IamMarkT <markt1084@yahoo.com>
2019-11-25 16:32:56 -0500

    added FutureBuilder ListView
    Shows songs when searched

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -16,40 +16,10 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
 
-  Future<void> getSearchResults() async {
-    currentToken.accessToken = await currentToken.getAccessToken();
-    String token = currentToken.accessToken;
-
-    print(currentToken.accessToken);
-    // String query = await getQuery();
-
-    try {
-      // make the request
-      Response response = await get(
-          'https://api.spotify.com/v1/search?q=kanye%20west&type=track',
-          headers: {HttpHeaders.authorizationHeader:
-          "Bearer $token"}
-      ); // enter access token after "Bearer"
-
-      var resultMap = jsonDecode(response.body);
-      print(resultMap);
-      searchOutput = SearchResult.fromJson(resultMap);
-
-      print(searchOutput.tracks.items[5].id);
-    }catch (e){
-      print('caught error: $e');
-    }
-
-    // get properties from data
-    // String message = resultMap.toString();
-    //  print(message);
-
-  }
-
   @override
   void initState() {
     super.initState();
-    getSearchResults();
+    // getSearchResult(query);
     //print(searchOutput.tracks.items[2].id);
     //await input.getSearchResults();
   }
@@ -63,20 +33,13 @@ class _SearchState extends State<Search> {
           IconButton(
             icon: Icon(Icons.search),
             onPressed: (){
+              currentToken.getAccessToken();
               showSearch(
                   context: context,
                   delegate: SongSearch());
             },
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            //input.getSearchResults();
-            //print(input.tracks.items[5].id);
-          });
-        },
       ),
     );
   }
@@ -85,18 +48,21 @@ class _SearchState extends State<Search> {
 
 class SongSearch extends SearchDelegate<Tracks>{
   final recentSearch = [];
+  int offset = 0;
+
+
 
   // DONE; WORKING CLEAR BUTTON
   @override
   List<Widget> buildActions(BuildContext context) {
     // TODO: implement buildActions
     // actions for app bar
+
     return [
       IconButton(
         icon: Icon(Icons.clear),
         onPressed: (){
           query = "";
-          print('safdfa');
         },
       )
     ];
@@ -119,23 +85,60 @@ class SongSearch extends SearchDelegate<Tracks>{
   }
 
   @override
-  Widget buildResults(BuildContext context) {
+  Widget buildResults(BuildContext context)  {
     // TODO: implement buildResults
     // show some result based on the selection
+    //getSearchResults(query);
 
+    if(query.isEmpty) {
+      return Container();
+    }else {
+      return FutureBuilder(
+        future: getSearchResults(query, offset, currentToken.accessToken),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              body: ListView.builder(
+                itemCount: searchOutput.tracks.items.length,
+                itemBuilder: (context, index){
+                  return Card(
 
-    //getSearchResults(inputQuery);
-    return Container(
-      height: 400,
-      width: 400,
-      child: Card(
-        color: Colors.red,
-        child: Center(
-          child: Text(query),
-        ),
-      ),
-    );
+                    margin: EdgeInsets.fromLTRB(5, 5, 5, 0),
+                    child: ListTile(
+                      //onTap: (){},
+                      title: Text(searchOutput.tracks.items[index].name),
+                      subtitle: Text(searchOutput.tracks.items[index].artists[0].name),
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(searchOutput.tracks.items[index].album.images[0].url),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: (){},
+                      ),
+                    ),
+                  );
+                },
+              ),
+              persistentFooterButtons: <Widget>[
+                FlatButton(
+                  child: Text('Next'),
+                  onPressed: (){
+
+                  },
+                ),
+                FlatButton(
+                  child: Text('Back'),
+                ),
+              ],
+            );
+          } else
+            return Center(child: CircularProgressIndicator());
+        },
+      );
+    }
   }
+
+  //Text(searchOutput.tracks.items[5].name),
 
   @override
   Widget buildSuggestions(BuildContext context) {
@@ -156,3 +159,31 @@ class SongSearch extends SearchDelegate<Tracks>{
   }
 
 }
+
+Future<void> getSearchResults(String query, int offset, String token) async {
+ // currentToken.accessToken = await currentToken.getAccessToken();
+  // String token = currentToken.accessToken;
+  print(currentToken.accessToken);
+
+  try {
+    // make the request
+    Response response = await get(
+        'https://api.spotify.com/v1/search?q=$query&type=track,artist&offset=$offset&limit=20',
+        headers: {HttpHeaders.authorizationHeader:
+        "Bearer $token"}
+    ); // enter access token after "Bearer"
+
+    var resultMap = jsonDecode(response.body);
+    print(resultMap);
+    searchOutput = SearchResult.fromJson(resultMap);
+
+    print(searchOutput.tracks.items[19].name);
+  }catch (e){
+    print('caught error: $e');
+  }
+
+  // get properties from data
+  // String message = resultMap.toString();
+  //  print(message);
+
+} // end of getSearchResults
