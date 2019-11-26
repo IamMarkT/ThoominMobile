ecbeec2464ae0311614ac36b99e3ce611f42d9cd
IamMarkT <markt1084@yahoo.com>
2019-11-26 03:10:38 -0500

    deleted search
    deleted search page and implemented into home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:thoomin/pages/search.dart';
+import 'package:http/http.dart';
+import 'package:thoomin/services/SearchResult.dart';
+import 'dart:convert';
+import 'package:thoomin/services/access_token.dart';
+import 'package:flutter/cupertino.dart';
+
+SearchResult searchOutput = SearchResult();
+AccessToken currentToken = AccessToken();
 
 class Home extends StatefulWidget {
   @override
@@ -9,7 +17,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int selectedPage = 0;
   final pageOptions = [
-    Search(),
     //currentlyplaying()
     SafeArea(child: Text('We THOOMIN!', style: TextStyle(fontSize: 36),)),
     SafeArea(child: Text('Landscape', style: TextStyle(fontSize: 36),)),
@@ -20,6 +27,29 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.purple[200],
 
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () { Scaffold.of(context).openDrawer(); },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+        title: Text("Thoomin Spotify"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              currentToken.getAccessToken();
+              showSearch(
+                  context: context,
+                  delegate: SongSearch());
+            },
+          ),
+        ],
+      ),
       body: pageOptions[selectedPage],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: selectedPage,
@@ -29,10 +59,6 @@ class _HomeState extends State<Home> {
           });
         },
         items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text('Search')
-          ),
           BottomNavigationBarItem(
               icon: Icon(Icons.play_arrow),
               title: Text('Currently Playing')
@@ -46,3 +72,133 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+class SongSearch extends SearchDelegate<Tracks>{
+  final recentSearch = [];
+  int offset = 0;
+
+  // DONE; WORKING CLEAR BUTTON
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    // actions for app bar
+
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: (){
+          query = "";
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    // leading icon on the left of the app bar
+
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: (){
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context)  {
+    // TODO: implement buildResults
+    // show some result based on the selection
+    //getSearchResults(query);
+
+    if(query.isEmpty) {
+      return Container();
+    }else {
+      return FutureBuilder(
+        future: getSearchResults(query, offset, currentToken.accessToken),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              body: ListView.builder(
+                itemCount: searchOutput.tracks.items.length,
+                itemBuilder: (context, index){
+                  return Card(
+
+                    margin: EdgeInsets.fromLTRB(5, 5, 5, 0),
+                    child: ListTile(
+                      //onTap: (){},
+                      title: Text(searchOutput.tracks.items[index].name),
+                      subtitle: Text(searchOutput.tracks.items[index].artists[0].name),
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(searchOutput.tracks.items[index].album.images[0].url),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: (){},
+                      ),
+                    ),
+                  );
+                },
+              ),
+              persistentFooterButtons: <Widget>[
+                FlatButton(
+                  child: Text('Next'),
+                  onPressed: (){
+
+                  },
+                ),
+                FlatButton(
+                  child: Text('Back'),
+                ),
+              ],
+            );
+          } else
+            return Center(child: CircularProgressIndicator());
+        },
+      );
+    }
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    // show when someone searches for anything
+    final suggestionList = recentSearch;
+
+    return ListView.builder(
+      itemBuilder: (context, index)=>ListTile(
+        onTap: (){
+          showResults(context);
+        },
+        leading: Icon(Icons.music_note),
+        title: Text(suggestionList[index]),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
+
+Future<void> getSearchResults(String query, int offset, String token) async {
+
+  try {
+    // make the request
+    Response response = await get(
+        'https://api.spotify.com/v1/search?q=$query&type=track,artist&offset=$offset&limit=20',
+        headers: {HttpHeaders.authorizationHeader:
+        "Bearer $token"}
+    ); // enter access token after "Bearer"
+
+    var resultMap = jsonDecode(response.body);
+    print(resultMap); //
+    searchOutput = SearchResult.fromJson(resultMap);
+
+    print(searchOutput.tracks.items[19].name); //
+  }catch (e){
+    print('caught error: $e');
+  }
+
+} // end of getSearchResults
